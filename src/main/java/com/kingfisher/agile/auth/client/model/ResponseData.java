ab039bd6c592aeca56b983e73672319aa8af9932
Sumangal-IN <mandas04@GBKTFDC013X0008.kits.kfplc.com>
2019-08-17 01:46:59 +0530

    refactor

--- src/main/java/com/kingfisher/agile/auth/client/model/ResponseData.java
+++ src/main/java/com/kingfisher/agile/auth/client/model/ResponseData.java
@@ -2,19 +2,13 @@ package com.kingfisher.agile.auth.client.model;
 
 import java.util.List;
 
-import org.codehaus.jackson.annotate.JsonProperty;
-
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
 public class ResponseData {
-
 	String jSessionID;
-
 	String username;
-
 	List<String> roles;
-
 }
